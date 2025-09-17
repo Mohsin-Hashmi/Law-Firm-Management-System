5ca211bb5653c68e60a6aec98cd788f01443e40c
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-09-17 17:21:07 +0500

    deploying backend

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -16,13 +16,10 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "mysql",
   },
   production: {
-    use_env_variable: "DB_URL", // Railway injects this
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // important for Railway
-      },
-    },
-  },
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  }
 };
