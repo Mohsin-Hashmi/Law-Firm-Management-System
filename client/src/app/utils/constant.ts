c4b1765285716f86de7c1496dde508910f3f8141
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-09-24 15:36:51 +0500

    Update BASE_URL to a static value and add Vercel rewrites configuration

--- client/src/app/utils/constant.ts
+++ client/src/app/utils/constant.ts
@@ -1,8 +1,5 @@
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = "/api";
 
-if (!BASE_URL) {
-  throw new Error("NEXT_PUBLIC_BASE_URL is not defined");
-}
 
 export default BASE_URL;
