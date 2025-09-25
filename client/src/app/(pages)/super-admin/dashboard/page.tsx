e41df10bc508daf87f236216f13d629956d5d513
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-09-25 16:22:59 +0500

    Implement user authentication check in SuperAdminDashboard to redirect unauthenticated users to login

--- client/src/app/(pages)/super-admin/dashboard/page.tsx
+++ client/src/app/(pages)/super-admin/dashboard/page.tsx
@@ -1,7 +1,18 @@
 import DashboardLayout from "@/app/components/DashboardLayout";
 import PlatformStats from "@/app/components/PlatformStats";
 import { ThemeProvider } from "next-themes";
+import { useAppSelector } from "@/app/store/hooks";
+import { RootState } from "@/app/store/store";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 export default function SuperAdminDashboard() {
+  const user = useAppSelector((state: RootState) => state.user.user);
+  const router = useRouter();
+  useEffect(() => {
+    if (!user) {
+      router.push("/auth/login");
+    }
+  }, [user, router]);
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <DashboardLayout>
