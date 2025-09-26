afdc96ed2b680a8c324de6757454889dc7f1ef8c
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-09-26 15:30:30 +0500

    Refactor the BASE_URL for production and development

--- client/src/app/utils/constant.ts
+++ client/src/app/utils/constant.ts
@@ -1,10 +1,2 @@
-const explicitApiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-const fallbackApiUrl =
-  process.env.NODE_ENV === "production"
-    ? "http://13.60.35.167:5000" 
-    : "http://localhost:5000";
-
-const BASE_URL = explicitApiUrl || fallbackApiUrl;
-
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 export default BASE_URL;
