fe7be2635a899ebb86489ace59d32abad692d35c
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-09-29 11:51:03 +0500

    configure uploads folder path

--- Server/src/app.js
+++ Server/src/app.js
@@ -63,7 +63,7 @@ app.use(
     methods: ["GET"],
     credentials: true,
   }),
-  express.static(path.join(__dirname, "uploads"))
+  express.static(path.join(__dirname, "../uploads"))
 );
 
 const PORT = process.env.PORT || 4000;
